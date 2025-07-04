c6bfc23c710d4b8c8c1fde62d69a618954f25492
MikoBie <profesor_floretu@op.pl>
2025-07-04 04:18:59 +0200

    Minor fix.

--- scripts/create_excel.py
+++ scripts/create_excel.py
@@ -2,23 +2,19 @@
 from pejk import EXCEL, DATA
 import pandas as pd
 import os
+import re
 
 # %%
+rgx = re.compile(r"\d+")
 lst_fls = [item for item in os.scandir(EXCEL) if "xlsx" in item.name]
-lst_fls = sorted(lst_fls, key=lambda x: x.name.split(".")[0])
+lst_fls = sorted(lst_fls, key=lambda x: int(rgx.search(x.name).group()))
 
 
 # %%
-def main() -> None:
-    writer = pd.ExcelWriter(DATA / "percenteges.xlsx", engine="xlsxwriter")
-    for item in lst_fls:
-        temp = pd.read_excel(item)
-        sheet_name = item.name.replace(".xlsx", "")
-        temp.to_excel(writer, sheet_name=sheet_name, index=False)
+writer = pd.ExcelWriter(DATA / "percenteges.xlsx", engine="xlsxwriter")
+for item in lst_fls:
+    temp = pd.read_excel(item)
+    sheet_name = item.name.replace(".xlsx", "")
+    temp.to_excel(writer, sheet_name=sheet_name, index=False)
 
-    writer.close()
-
-
-# %%
-if __name__ != "__main__":
-    main()
+writer.close()
